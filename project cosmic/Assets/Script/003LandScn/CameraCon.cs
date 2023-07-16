d210df1266b434326a33255b52072c054795e7a9
sih <81147612+nsih@users.noreply.github.com>
2023-07-16 22:02:19 +0900

    camera moving Update
    마우스 중심 -> 플레이어 중심

--- project cosmic/Assets/Script/003LandScn/CameraCon.cs
+++ project cosmic/Assets/Script/003LandScn/CameraCon.cs
@@ -27,15 +27,20 @@ public class CameraCon : MonoBehaviour
 
     void Chasing()
     {
-        Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition); 
         Vector3 playerPosition = player.transform.position;
+        Vector3 targetPosition = playerPosition;
 
-        Vector3 targetPosition = (mousePosition + playerPosition) / 10f;
-        targetPosition.z = transform.position.z;
+        // 마우스 위치와 플레이어 사이의 벡터
+        Vector3 mouseOffset = Camera.main.ScreenToWorldPoint(Input.mousePosition) - playerPosition;
+
+        // Z 축 값을 유지하여 플레이어 중심 위치에 마우스 벡터의 10%를 더함
+        targetPosition += new Vector3(mouseOffset.x * 0.1f, mouseOffset.y * 0.1f, 0f);
 
         // 부드러운 이동을 위한 보간
         Vector3 smoothPosition = Vector3.Lerp(transform.position, targetPosition, cameraSpeed * Time.deltaTime);
-
+        
+        // Z 축 값을 유지하여 카메라 위치 설정
+        smoothPosition.z = transform.position.z;
         transform.position = smoothPosition;
     }
 }
