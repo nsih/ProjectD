6025ea64dec210ef711f0e81b992a23d868fe501
sih <81147612+nsih@users.noreply.github.com>
2023-07-18 22:14:34 +0900

    Update Artifact Btn

--- project cosmic/Assets/Script/003LandScn/BtnMenuBoxe/ArtifactBtnCon.cs
+++ project cosmic/Assets/Script/003LandScn/BtnMenuBoxe/ArtifactBtnCon.cs
@@ -8,7 +8,7 @@ public class ArtifactBtnCon : MonoBehaviour
     GameObject gameManager;
     GameObject landUiCanvas;
     GameObject pnlBackGround;
-    GameObject ritualPopup;
+    GameObject artifactPopup;
 
 
     Button btnClose;
@@ -23,8 +23,8 @@ public class ArtifactBtnCon : MonoBehaviour
         gameManager = GameObject.Find("GameManager");
         landUiCanvas = GameObject.Find("LandUICanvas");
         pnlBackGround = GameObject.Find("PnlBackGround");
-        ritualPopup = pnlBackGround.transform.Find("RitualPopup").gameObject;
-        btnClose = ritualPopup.transform.Find("BtnClose").gameObject.GetComponent<Button>();
+        artifactPopup = pnlBackGround.transform.Find("ArtifactPopup").gameObject;
+        btnClose = artifactPopup.transform.Find("BtnClose").gameObject.GetComponent<Button>();
 
         ButtonText = this.gameObject.transform.GetChild(0).gameObject;
         ButtonImage = this.gameObject.transform.GetChild(1).gameObject;
@@ -36,7 +36,7 @@ public class ArtifactBtnCon : MonoBehaviour
     }
 
 
-    public void OnClickRitual()
+    public void OnClickArtifact()
     {
         OpenPopup();
     }
@@ -44,28 +44,20 @@ public class ArtifactBtnCon : MonoBehaviour
 
     void ShowButtonImage()
     {
-        if(GameManager.isActionPhase && GameManager.actionStack > 0)
-        {
-            ButtonImage.GetComponent<Image>().color = Color.white;
-            ButtonText.GetComponent<TextMeshProUGUI>().text = "의식";
-        }
-        else
-        {
-            ButtonImage.GetComponent<Image>().color = Color.clear;
-            ButtonText.GetComponent<TextMeshProUGUI>().text = "<color=#FF0000>의식</color>";
-        }
+        ButtonImage.GetComponent<Image>().color = Color.white;
+        ButtonText.GetComponent<TextMeshProUGUI>().text = "유물";
     }
 
     void OpenPopup()
     {
-        ritualPopup.SetActive(true);
+        artifactPopup.SetActive(true);
 
         btnClose.onClick.AddListener(ClosePopup);
     }
 
     void ClosePopup()
     {
-        ritualPopup.SetActive(false);
+        artifactPopup.SetActive(false);
 
         btnClose.onClick.RemoveAllListeners();
     }
