310972b941ba51c90b4003c4cddffa20fa913ab3
sih <81147612+nsih@users.noreply.github.com>
2023-11-21 04:42:50 +0900

    Update BGMManager.cs

--- project cosmic/Assets/Script/000General/Sound/BGMManager.cs
+++ project cosmic/Assets/Script/000General/Sound/BGMManager.cs
@@ -4,5 +4,45 @@ using UnityEngine;
 
 public class BGMManager : MonoBehaviour
 {
-    
+    private AudioSource bgmAudioSource;
+    public AudioClip[] bgmClips;
+    private int currentBGMIndex = -1;
+    [Range(0f, 1f)]
+    public float bgmVolume = 1f;
+
+    private void Awake()
+    {
+        bgmAudioSource = gameObject.AddComponent<AudioSource>();
+        bgmAudioSource.loop = true;
+    }
+
+    public void PlayBGM(int bgmIndex)
+    {
+        if (bgmIndex >= 0 && bgmIndex < bgmClips.Length)
+        {
+            if (bgmIndex != currentBGMIndex)
+            {
+                currentBGMIndex = bgmIndex;
+                bgmAudioSource.clip = bgmClips[currentBGMIndex];
+                bgmAudioSource.volume = bgmVolume;
+                bgmAudioSource.Play();
+            }
+        }
+        else
+        {
+            Debug.LogWarning("Invalid BGM index");
+        }
+    }
+
+    public void StopBGM()
+    {
+        bgmAudioSource.Stop();
+        currentBGMIndex = -1;
+    }
+
+    public void SetBGMVolume(float volume)
+    {
+        bgmVolume = Mathf.Clamp01(volume);
+        bgmAudioSource.volume = bgmVolume;
+    }
 }
