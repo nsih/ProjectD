a7d0c5729e476977bb5d37cffdf6d24ccd9b6d0c
sih <81147612+nsih@users.noreply.github.com>
2023-12-12 14:23:34 +0900

    action

--- project cosmic/Assets/Script/003LandScn/BtnMenuBox/ActionBtnCon.cs
+++ project cosmic/Assets/Script/003LandScn/BtnMenuBox/ActionBtnCon.cs
@@ -5,7 +5,7 @@ using UnityEngine.UI;
 using TMPro;
 using UnityEngine.EventSystems;
 
-public class ActionBtnCon : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+public class ActionBtnCon : MonoBehaviour//, IPointerEnterHandler, IPointerExitHandler
 {
     GameObject gameManager;
     GameObject landUiCanvas;
@@ -14,13 +14,14 @@ public class ActionBtnCon : MonoBehaviour, IPointerEnterHandler, IPointerExitHan
 
     Button btnClose;
 
-
+    
     GameObject ButtonText;
     GameObject ButtonImage;
 
+    /*
     private Color normalColor;
     private Color hoverColor;
-
+    */
 
     void Start()
     {
@@ -32,18 +33,20 @@ public class ActionBtnCon : MonoBehaviour, IPointerEnterHandler, IPointerExitHan
         ButtonText = this.gameObject.transform.GetChild(0).gameObject;
         ButtonImage = this.gameObject.transform.GetChild(1).gameObject;
 
+        /*
         normalColor = GetComponent<Image>().color;
         float r = Mathf.Clamp(normalColor.r - 0.2f, 0f, 1f);
         float g = Mathf.Clamp(normalColor.g - 0.2f, 0f, 1f);
         float b = Mathf.Clamp(normalColor.b - 0.2f, 0f, 1f);
         hoverColor = new Color(r, g, b, normalColor.a);
+        */
     }
 
     void Update ()
     {
         //ShowButtonImage();
     }
-
+    /*
     public void OnPointerEnter(PointerEventData eventData)
     {
         GetComponent<Image>().color = hoverColor;
@@ -53,26 +56,12 @@ public class ActionBtnCon : MonoBehaviour, IPointerEnterHandler, IPointerExitHan
     {
         GetComponent<Image>().color = normalColor;
     }
+    */
 
 
     public void OnClickAction()
     {
         OpenPopup();
-    }
-
-
-    void ShowButtonImage()
-    {
-        if(GameManager.isEventEnd && PlayerInfo.currentAP > 0)
-        {
-            ButtonImage.GetComponent<Image>().color = Color.white;
-            ButtonText.GetComponent<TextMeshProUGUI>().text = "행동";
-        }
-        else
-        {
-            ButtonImage.GetComponent<Image>().color = Color.clear;
-            ButtonText.GetComponent<TextMeshProUGUI>().text = "<color=#FF0000>행동</color>";
-        }
     }
 
     void OpenPopup()
